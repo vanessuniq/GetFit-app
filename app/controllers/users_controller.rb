9f2e883fcde45b0e0d6a8bc009cd0b1502837898
vanessuniq <vanessuniq@gmail.com>
2020-05-15 01:44:51 -0400

    created user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
     
+    get '/registration/signup' do
+        if logged_in?
+            redirect "/user/profile/#{current_user.username}"
+        else
+            erb :'users/signup'
+        end
+    end
+
+    post '/registration' do
+        @user = User.new(params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect "/profile/#{current_user.username}"
+        else
+            @errors = @user.errors.full_messages
+            redirect '/registration/signup'
+        end
+    end
+
+    
+
 end
