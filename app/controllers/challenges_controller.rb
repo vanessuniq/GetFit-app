4cdc7e803e844283200f175ece341097ca9322cc
vanessuniq <vanessuniq@gmail.com>
2020-05-17 09:15:46 -0400

    add new challenge route and forms

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,3 +1,15 @@
 class ChallengesController < ApplicationController
-    
+    get '/challenges' do
+        @challenges = Challenge.all
+        erb :'challenges/show_all'
+    end
+
+    get '/challenges/new' do
+        if logged_in?
+            @types = Type.all
+            erb :'challenges/create'
+        else
+            redirect '/session/login'
+        end
+    end
 end
