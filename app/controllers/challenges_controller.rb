be1ee01f792cf83f1edea8c1a97609a8a720890f
vanessuniq <vanessuniq@gmail.com>
2020-05-17 10:38:42 -0400

    added the post rout for new challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -12,4 +12,30 @@ class ChallengesController < ApplicationController
             redirect '/session/login'
         end
     end
+
+    post '/challenges' do
+        if logged_in?
+            n = (params[:challenge][:duration]).to_i
+            num_sets = (params[:sets]).to_i
+            up = (params[:set_increment]).to_i
+
+            @challenge = current_user.challenges.build(params[:challenge])
+            @type = Type.find(params[:type])
+            @challenge.type = @type 
+            n.times do
+                c = 1
+                @challenge.days << Day.new(name: "day#{c}", sets: num_sets)
+                c += 1
+                num_sets += up
+            end
+            if @challenge.save
+                redirect "/challenges/#{@challenge.id}"
+            else
+              @errors = @challenge.errors.full_messages
+              erb :'challenges/create'  
+            end
+        else
+          redirect '/session/login'  
+        end
+    end
 end
