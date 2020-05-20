3df8ad962555ec3f5387019eab4b024e9c3818f1
vanessuniq <vanessuniq@gmail.com>
2020-05-20 03:07:42 -0400

    updated users routes name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-    
+
+    get '/users' do
+        redirect '/'
+    end
+
     get '/registration/signup' do
         if logged_in?
-            redirect "/user/profile/#{current_user.username}"
+            redirect "/users/#{current_user.username}"
         else
+            @errors = session.delete(:errors)
             erb :'users/signup'
         end
     end
@@ -12,44 +17,45 @@ class UsersController < ApplicationController
         @user = User.new(params)
         if @user.save
             session[:user_id] = @user.id
-            redirect "/profile/#{current_user.username}"
+            redirect "/users/#{current_user.username}"
         else
-            @errors = @user.errors.full_messages
+            session[:errors] = @user.errors.full_messages
             redirect '/registration/signup'
         end
     end
 
-    get '/session/login' do
-        if logged_in?
-            redirect "/profile/#{current_user.username}" 
-        else
-            erb :'users/login'
-        end
-    end
-
-    post '/session' do
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect "/profile/#{current_user.username}"
-        else
-            @errors = "Invalid username or password"
-            erb :'users/login'
-        end
-    end
-
-    get '/session/logout' do
-        session.clear
-        redirect '/'
-    end
-
-    get '/profile/:username' do
+    get '/users/:username' do
         if logged_in?
             @user = current_user
             erb :'users/profile'
         else
-            redirect '/session/login'
+            redirect '/sessions/login'
         end
+    end
+
+    get '/sessions/login' do
+        if logged_in?
+            redirect "/users/#{current_user.username}" 
+        else
+            @errors = session.delete(:errors)
+            erb :'users/login'
+        end
+    end
+
+    post '/sessions' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{current_user.username}"
+        else
+            session[:errors] = "Invalid username or password"
+            redirect '/sessions/login'
+        end
+    end
+
+    get '/sessions/logout' do
+        session.clear
+        redirect '/'
     end
 
 end
